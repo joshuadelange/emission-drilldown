95a85efda11b74992b23ec7336c7ae9ea32eabc0
Joshua de Lange <joshua.delange@gmail.com>
2023-10-02 17:57:46 +0200

    refactor: small code clean up

--- main.js
+++ main.js
@@ -5,6 +5,5 @@ import { initPieChart } from './src/components/pie-chart.js'
 init()
 
 function init () {
-  const pieChartElement = document.getElementById('pie-chart')
-  initPieChart(pieChartElement)
+  initPieChart(document.getElementById('pie-chart'))
 }
